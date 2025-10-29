f825d9592d0b6fe57e6a1654cd44d321ad6aa9c3
Lewis Moten <lewismoten@gmail.com>
2025-10-29 15:53:18 -0400

    read xlsx file

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world')
+import xlsx from 'xlsx';
+
+const file = '/Users/lewismoten/Library/CloudStorage/Dropbox/SPL Bibliostat/2024 Virginia Public Library Survey_10-27-25_100856.xlsx'
+const workbook = xlsx.readFile(file);
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+const data = xlsx.utils.sheet_to_json(sheet);
+console.log(data);
